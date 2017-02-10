fec476647ea4bf09809401a4bf7cce9b2c7ae47f
Valentin Fries <contact@fries.io>
2017-02-10 14:33:16 +0100

    dev
     - Project documentation

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,26 @@
-//! The root crate for the Locksidian project.
+//! The root crate for the `Locksidian` project.
+//!
+//! ## Contributors
+//!
+//! [Valentin Fries](https://www.fries.io) ([GitHub](https://github.com/MrKloan)) & Aurélien Duval
+//! ([GitHub](https://github.com/acid-killa666))
 //!
 //! ## Overview
 //!
 //! Locksidian is a pure [Rust](https://www.rust-lang.org/) implementation of the
-//! [blockchain](https://en.wikipedia.org/wiki/Blockchain_(database)) technology.
+//! [blockchain](https://en.wikipedia.org/wiki/Blockchain_(database)) technology, and was developed
+//! as a study project during our first year of Master degree in *Software Architecture*.
+//!
+//! Our objective is to provide the ability to store and certify the existence and integrity of any
+//! JSON document by storing and cryptographically signing them inside of the `Locksidian`
+//! blockchain.
+//!
+//! All the interactions between the peers of the peer-to-peer network is done using the HTTP(S)/1.1
+//! protocol through the use of REST APIs.
+//!
+//! ## Deeper into the rabbit hole
+//!
+//! *TODO*
 
 // Third-party dependencies
 extern crate time;
