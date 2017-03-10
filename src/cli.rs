7d3e1b0df70adfcb4760a1c15e32ae0599bf78ed
Valentin Fries <contact@fries.io>
2017-03-10 11:31:10 +0100

    Issue #9
     - Ease the process of creating a new Repository structure using the
       crud_repository! macro

--- src/cli.rs
+++ src/cli.rs
@@ -7,7 +7,7 @@ use getopts::Matches;
 
 use opts;
 use api;
-use persistence;
+use persistence::*;
 
 pub fn handle(matches: Matches) {
     if matches.opt_present("help") {
@@ -30,9 +30,9 @@ pub fn handle(matches: Matches) {
 fn daemon(opt_addr: Option<String>) {
     match opt_addr {
         Some(listen_addr) => {
-            match persistence::get_connection(persistence::DATABASE_PATH) {
+            match get_connection(DATABASE_PATH) {
                 Ok(connection) => {
-                    persistence::setup_database(&connection).expect("Unable to initialize the database schemas");
+                    setup_database(&connection).expect("Unable to initialize the database schemas");
 
                     let server = api::Server::new(listen_addr);
                     server.start(api::router());
