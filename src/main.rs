a5544fddcac8dd145dcb89e44535e1b55473d74f
Valentin Fries <contact@fries.io>
2017-03-14 20:37:08 +0100

    Issue #11
     - Second Proof of Work draft using data size in bytes and the number of
       mined blocks ina given window of time
     - PROTECTED flag in order to prevent anyone to store documents using your
       node

--- src/main.rs
+++ src/main.rs
@@ -172,77 +172,105 @@
 //!     data: String,           // JSON document                            | Block data
 //!
 //!     data_hash: String,      // SHA512 data checksum                     | Block Header
+//!     signature: String,      // Signature of the data_hash               |
 //!     timestamp: u64,         // Creation timestamp of the block          |
 //!     nonce: u32,             // Proof of Work solution                   |
 //!     previous: String,       // Hash of the previous block in the chain  |
 //!
 //!     hash: String,           // SHA512 Block Header checksum                                         | Block metadata
-//!     next: String,           // Hash of the next block in the chain                                  |
 //!     height: u64,            // Block index relative to the main chain                               |
-//!                                                                                                     |
+//!     author: String          // Identity hash of the block author                                    |
+//!     next: String,           // Hash of the next block in the chain                                  |
 //!     received_at: u64,       // Reception timestamp of the block by the sending peer                 |
-//!     received_from: String   // Identity hash of the peer from which this block has been received    |
-//!                                                                                                     |
-//!     author: String,         // Identity hash of the block author                                    |
-//!     signature: String,      // Signature of the block's hash using the author Private Key           |
+//!     received_from: String,  // Identity hash of the peer from which this block has been received    |
 //! }
 //! ```
 //!
 //! In order to register a new JSON document into the `Locksidian` blockchain, you just have to `POST`
 //! it to the `/blocks` endpoint of any node of the peer-to-peer network.
 //!
-//! The origin of the requests `POST`ed on this endpoints are not checked: anyone can submit a
-//! document to the blockchain!
+//! By default, the origin of the requests `POST`ed on this endpoints are not checked: anyone can
+//! submit a document to the blockchain!
 //!
-//! A new `Block` structure is initialized with the current timestamp and the JSON document as its
-//! `data` field and the document's size in bytes as its `bytes` field.
+//! A new `Block` structure is initialized with the current `timestamp`, the JSON document as its
+//! `data` field and the document's SHA512 checksum as its `data_hash` field.
 //!
-//! Then the node will calculate the SHA512 checksum of the `data` and store it in the `hash` field.
-//! Then it will browse the blockchain, searching for a block of the exact same checksum.
+//! The node will then browse the blockchain, searching for a block of the exact same checksum.
+//! If a block *does* exists with the exact same `data_hash` anywhere in the chain, the node will throw
+//! a `409 Conflict` and send the existing block `hash` in the HTTP response.
 //!
-//! If a block *does exists* with the exact same `hash` anywhere in the chain, the node will throw
-//! a `409 Conflict` and send the checksum value in the HTTP response.
-//!
-//! If there is no block with the same checksum in the chain, the following fields of the `Block`
+//! If there is no block with the same `data_hash` checksum in the chain, the following fields of the `Block`
 //! structure are initialized (with `HEAD` the Block representing the current head of the blockchain):
 //!
 //! ```text
-//! block.received_at = block.timestamp
+//! block.signature = {block.data_hash signed using the node's private key}
 //! block.previous = HEAD.hash
 //! block.next = (empty string)
-//! HEAD.next = block.hash
+//!
 //! block.height = HEAD.height + 1
+//! block.received_at = block.timestamp
 //! block.received_from = (current node identity)
+//! block.author = (current node identity)
 //! ```
 //!
 //! But in order to add this new block into the blockchain, the node has to solve a
-//! [Proof of Work](https://en.bitcoin.it/wiki/Proof_of_work) challenge. In `Locksidian`, the
-//! **Proof of Work** is implemented in the following way (**TODO**: completely revamp the PoW
-//! target calculation):
+//! [Proof of Work](https://en.bitcoin.it/wiki/Proof_of_work) challenge.
 //!
-//!  - [*deprecated*] The 8 [most significant bits](https://en.wikipedia.org/wiki/Most_significant_bit) (the first
-//!    byte) of the block's hash is used to compute the PoW objective: the number of leading `0`
-//!    required in the PoW hash. The number is calculated as follows: `{byte} % 5 + 1`, which gives
-//!    us a window of `1` to `5` leading zeros, which feels a good compromise for this prototype.
+//! As you may have understood, all the checksum in `Locksidian` are computed using the SHA512
+//! hashing algorithm - [which is faster than SHA256 on 64-bit devices](http://crypto.stackexchange.com/questions/26336/sha512-faster-than-sha256).
+//! The base 10 value of a SHA512 hash could (theoretically) exist in a range of `0` to `2^512`.
+//! As the main objective of the `Locksidian` blockchain is to allow anyone to store JSON documents
+//! of *any size*, we thought about using the size in bytes of the data stored in a block in order to
+//! determine the difficulty of its mining process.
+//!
+//! In `Locksidian`, the **Proof of Work** is implemented in the following way:
+//!
+//!  - The size (in bytes) of the block's data is divided by `32` and then floored. As an example,
+//!    if the JSON document is composed of 135 chars, `135/32 ~= 4.22 = 4`. This result is then used
+//!    to *decrease* the number of bits used to calculate the PoW *target*. In our example, a result
+//!    of `4` will produce `512 - 4 = 508` bits. At the end of this first step, the Proof of Work
+//!    target value is `2^508`. This mechanism is used to prevent malevolent nodes or individuals to
+//!    spam the network with huge documents, as the computation power required to mine the block
+//!    will increase exponentially every `32` bytes.
+//!
+//!  - In order to prevent a DDoS attack as well as spamming little documents onto the blockchain,
+//!    the number of blocks mined in the last 10 minutes by the current node will be counted
+//!    (using its `Identity` hash, stored in the `author` field of each block) and deduced from the
+//!    number of bits allocated to the PoW target. Say our node has mined 5 blocks in the last 10
+//!    minutes when computing our document: `508 - 5 = 503`, so the updated PoW target is now `2^503`.
 //!
 //!  - The `nonce`, an unsigned 32 bit number, is initialized to `0` and stored in the Block Header
-//!    whose SHA512 checksum is computed. If the checksum value is lower then the PoW objective, then
+//!    whose SHA512 checksum is computed. If the checksum value is lower than the PoW target, then
 //!    the current `nonce` value is stored in the structure. If the PoW is not satisfied, the `nonce`
 //!    is incremented and the payload checksum is recomputed; loop until the PoW is solved.
 //!
-//! Once the PoW is solved, the block hash is signed using the `Identity` RSA Private Key to prove
-//! the ownership of the block and the remaining fields of the `Block` structure are initialized:
+//! Once the PoW is solved, the nonce is stored in the Block Header whose SHA512 checksum is computed
+//! and stored into the Block Metadata. Finally, the `next` field of the current HEAD block is updated:
 //!
 //! ```text
 //! block.nonce = {nonce}
-//! block.author = {current node identity}
-//! block.signature = {block.hash signed using the node's private key}
+//! block.hash = {Block Header SHA512 checksum}
+//!
+//! HEAD.next = block.hash
 //! ```
 //!
 //! The block is then stored in the node's registry and the HEAD reference is updated.
 //!
 //! Finally, the node will broadcast the newly forged block to all its peers on the peer-to-peer
 //! network.
+//!
+//! ### "Protecting" your node
+//!
+//! As explained earlier, anyone can publish any JSON document to a `Locksidian` node... *by default*.
+//!
+//! In fact, you have the possibility to "protect" your node using either the `--protected` command
+//! line argument or the `LS_PROTECTED` environment variable.
+//!
+//! When running in protected mode, the node will check for a valid signature matching its current
+//! `Identity` when receiving a new JSON document on its `/blocks` endpoint.
+//!
+//! If there is no signature provided or if the signature does not match, a `403 Unauthorized` HTTP
+//! status will be returned to the client.
 //!
 //! ### Block replication 101
 //!
@@ -251,17 +279,17 @@
 //!
 //! ```json
 //! {
-//!     "data": {Block's data},
-//!     "bytes": {Data size in bytes},
-//!     "hash": {Block's hash},
-//!     "timestamp": {Block's timestamp},
+//!     "data": {Block's data},                     | Block Data
 //!
-//!     "nonce": {Proof of Work solution},
-//!     "author": {Identity of the block's author},
-//!     "signature": {Signature proving ownership},
+//!     "data_hash": {Data checksum},               | Block Header
+//!     "signature": {data_hash signature},         |
+//!     "timestamp": {Block's timestamp},           |
+//!     "nonce": {Proof of Work solution},          |
+//!     "previous": {Previous block hash},          |
 //!
-//!     "previous": {Previous block hash},
-//!     "height": {Block height}
+//!     "hash": {Block Header checksum}             | Block Metadata
+//!     "height": {Block height},                   |
+//!     "author": {Identity of the block's author}  |
 //! }
 //! ```
 //!
@@ -281,9 +309,9 @@
 //! Proof of Work is correct, the empty structure values are initialized:
 //!
 //! ```text
+//! block.next = (empty string)
 //! block.received_at = (current timestamp)
 //! block.received_from = (requester's identity hash)
-//! block.next = (empty string)
 //! ```
 //!
 //! The block referenced by the `previous` field will then be searched in the registry. If it is not
