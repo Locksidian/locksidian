01fcbaeb32517087571853643e34d7b094ad1650
Aurelien Duval <aurelien.duval6@gmail.com>
2017-04-09 19:47:06 +0200

    Implemented RSA algorithm in its first version

--- src/sec/asymetric_cypher.rs
+++ src/sec/asymetric_cypher.rs
@@ -1,8 +1,14 @@
+//! Asymetric Cypher trait
+//!
+//! # Asymetric Cypher
+//!
+//! Defines the basics of an asymetric cypher algorithm as supported by Locksidian's blockchain
+
 use std::path::Path;
 
 pub type PrivateKey = Vec<u8>;
 pub type PublicKey = Vec<u8>;
-pub type Passphrase = Vec<u8>;
+pub type Passphrase = [u8];
 pub type Cypher = Vec<u8>;
 pub type Message = Vec<u8>;
 pub type SignedMessage = Vec<u8>;
@@ -21,7 +27,7 @@ pub trait AsymetricCypher {
     /// ```rust
     /// cypher.generate_key_pair(2048);
     /// ```
-    fn generate_key_pair(&self, size: u32);
+    fn generate_key_pair(&mut self, size: u32);
 
     /// Encrypts a message using public key
     /// ```rust
@@ -39,55 +45,55 @@ pub trait AsymetricCypher {
     /// ```rust
     /// cypher.decrypt_with_public_key(message);
     /// ```
-    fn decrypt_with_public_key(&self, message : &Message) -> String;
+    fn decrypt_with_public_key(&self, cypher : &Cypher) -> Message;
 
     /// Decrypts a message using private key
     /// ```rust
     /// cypher.decrypt_with_private_key(message);
     /// ```
-    fn decrypt_with_private_key(&self, message : &Message) -> String;
+    fn decrypt_with_private_key(&self, cypher : &Cypher) -> Message;
 
     /// Imports public key from PEM file
     /// ```rust
-    /// cypher.import_public_key_from_file(filepath);
+    /// cypher.import_public_key(filepath);
     /// ```
-    fn import_public_key_from_file(&self, path : &Path) -> PublicKey;
+    fn import_public_key(&mut self, path: &Path);
 
     /// Imports private key from PEM file
     /// ```rust
-    /// cypher.import_private_key_from_file(filepath);
+    /// cypher.import_private_key(filepath);
     /// ```
-    fn import_private_key_from_file(&self, path : &Path) -> PrivateKey;
+    fn import_private_key(&mut self, path : &Path);
 
     /// Imports public key from PEM file with a passphrase
     /// ```rust
-    /// cypher.import_public_key_from_file(filepath, passphrase);
+    /// cypher.import_public_key(filepath, passphrase);
     /// ```
-    fn import_private_key_from_file_with_passphrase(&self, path : &Path, passphrase : Passphrase) -> PrivateKey;
-
-    /// Imports key from PEM files
-    /// ```rust
-    /// cypher.import_keys_from_file(filepath);
-    /// ```
-    fn import_keys_from_file(&self, path : &Path) -> KeyPair;
+    fn import_private_key_with_passphrase(&mut self, path : &Path, passphrase : &Passphrase);
 
     /// Exports public key from PEM file
     /// ```rust
-    /// cypher.export_public_key_from_file(filepath);
+    /// cypher.export_public_key(filepath);
     /// ```
-    fn export_public_key_to_file(&self, path : &Path);
+    fn export_public_key(&self, path : &Path);
 
     /// Exports private key from PEM file
     /// ```rust
-    /// cypher.export_private_key_from_file(filepath);
+    /// cypher.export_private_key(filepath);
     /// ```
-    fn export_private_key_to_file(&self, path : &Path);
+    fn export_private_key(&self, path : &Path);
+
+    /// Exports public key from PEM file with a passphrase
+    /// ```rust
+    /// cypher.export_private_key(filepath, passphrase);
+    /// ```
+    fn export_private_key_with_passphrase(&self, path: &Path, passphrase: &Passphrase);
 
     /// Exports key from PEM files
     /// ```rust
-    /// cypher.export_keys_from_file(filepath);
+    /// cypher.export_keys(filepath);
     /// ```
-    fn export_keys_to_file(&self, path : &Path);
+    fn export_key_pair(&self, path : &Path);
 
     /// Checks message with given signature
     /// ```rust
--- src/sec/rsa.rs
+++ src/sec/rsa.rs
@@ -1,4 +1,48 @@
-//! RSA algorithms, used to encrypt and decrypt data
+//! RSA implementation
+//!
+//! # RSA implementation for Locksidian
+//!
+//! Defines an RSA algorithm entry point as supported by Locksidian's blockchain
+//!
+//! Usage:
+//!
+//! # Generate a key pair :
+//!
+//! ```rust
+//! rsa.generate_key_pair(size);
+//! ```
+//!
+//! # Encrypt with public key and decrypt with private one :
+//!
+//! ```rust
+//! cypher = rsa.encrypt_with_public_key(message);
+//! message = rsa.decrypt_with_private_key(cypher);
+//! ```
+//!
+//! # Encrypt with private key and decrypt with public one :
+//!
+//! ```rust
+//! cypher = rsa.encrypt_with_private_key(message);
+//! message = rsa.decrypt_with_public_key(cypher);
+//! ```
+//!
+//! # Import / Export keys :
+//!
+//! ```rust
+//! rsa.import_public_key(path);
+//! rsa.import_public_key(path);
+//! rsa.export_public_key(path);
+//! rsa.export_public_key(path);
+//! rsa.export_key_pair(path);
+//! ```
+//!
+//! # Signatures :
+//!
+//! ```rust
+//! isOk = rsa.verify_signature(message, signature);
+//! signedMessage = rsa.sign(message);
+//! ```
+
 extern crate openssl;
 
 use sec::asymetric_cypher::*;
@@ -6,6 +50,11 @@ use self::openssl::rsa::Rsa;
 use self::openssl::pkey::PKey;
 use self::openssl::sign::Signer;
 use self::openssl::sign::Verifier;
+use self::openssl::rsa::Padding;
+use self::openssl::symm::Cipher;
+use self::openssl::rsa::PKCS1_PADDING;
+use std::fs::File;
+use std::io::Write;
 
 use std::path::Path;
 use self::openssl::hash::MessageDigest;
@@ -28,52 +77,77 @@ impl RsaService {
 }
 
 impl AsymetricCypher for RsaService {
-    fn generate_key_pair(&self, size: u32) {
-        unimplemented!()
-        //self.pkey.rsa().generate(size);
+    fn generate_key_pair(&mut self, size: u32) {
+        let rsa = Rsa::generate(size).unwrap();
+        self.pkey = PKey::from_rsa(rsa).unwrap();
     }
+
     fn encrypt_with_public_key(&self, message : &Message) -> Cypher {
-        unimplemented!()
+        let mut cypher : Cypher = Cypher::new();
+        self.pkey.rsa().unwrap().public_encrypt(message, &mut cypher, PKCS1_PADDING);
+        return cypher;
     }
 
     fn encrypt_with_private_key(&self, message : &Message) -> Cypher {
-        unimplemented!()
+        let mut cypher : Cypher = Cypher::new();
+        self.pkey.rsa().unwrap().private_encrypt(message, &mut cypher, PKCS1_PADDING);
+        return cypher;
     }
 
-    fn decrypt_with_public_key(&self, message : &Message) -> String {
-        unimplemented!()
+    fn decrypt_with_public_key(&self, cypher : &Cypher) -> Message {
+        let mut message : Cypher = Cypher::new();
+        self.pkey.rsa().unwrap().public_decrypt(cypher, &mut message, PKCS1_PADDING);
+        return message;
     }
 
-    fn decrypt_with_private_key(&self, message : &Message) -> String {
-        unimplemented!()
+    fn decrypt_with_private_key(&self, cypher : &Cypher) -> Message {
+        let mut message : Cypher = Cypher::new();
+        self.pkey.rsa().unwrap().private_decrypt(cypher, &mut message, PKCS1_PADDING);
+        return message;
     }
 
-    fn import_public_key_from_file(&self, path: &Path) -> PublicKey {
-        unimplemented!()
+    fn import_public_key(&mut self, path: &Path) {
+        let path_string = path.to_str().unwrap().as_bytes();
+        self.pkey = PKey::public_key_from_pem(path_string).unwrap();
     }
 
-    fn import_private_key_from_file(&self, path: &Path) -> PrivateKey {
-        unimplemented!()
+    fn import_private_key(&mut self, path: &Path) {
+        let path_string = path.to_str().unwrap().as_bytes();
+        self.pkey = PKey::private_key_from_pem(path_string).unwrap();
     }
 
-    fn import_private_key_from_file_with_passphrase(&self, path: &Path, passphrase: Passphrase) -> PrivateKey {
-        unimplemented!()
+    fn import_private_key_with_passphrase(&mut self, path: &Path, passphrase: &Passphrase) {
+        let path_string = path.to_str().unwrap().as_bytes();
+        self.pkey = PKey::private_key_from_pem_passphrase(path_string, passphrase).unwrap();
     }
 
-    fn import_keys_from_file(&self, path: &Path) -> KeyPair {
-        unimplemented!()
+    fn export_public_key(&self, path: &Path) {
+        let path_string = path.to_str().unwrap();
+        let key : Vec<u8> = self.pkey.public_key_to_pem().unwrap();
+        let mut output_file = File::create(path_string).unwrap();
+        output_file.write_all(key.as_slice());
     }
 
-    fn export_public_key_to_file(&self, path: &Path) {
-        unimplemented!()
+    fn export_private_key(&self, path: &Path) {
+        let path_string = path.to_str().unwrap();
+        let key : Vec<u8> = self.pkey.private_key_to_pem().unwrap();
+        let mut output_file = File::create(path_string).unwrap();
+        output_file.write_all(key.as_slice());
     }
 
-    fn export_private_key_to_file(&self, path: &Path) {
-        unimplemented!()
+    fn export_private_key_with_passphrase(&self, path: &Path, passphrase: &Passphrase) {
+        let path_string = path.to_str().unwrap();
+        let key : Vec<u8> = self.pkey.private_key_to_pem_passphrase(Cipher::aes_256_ecb(), passphrase).unwrap();
+        let mut output_file = File::create(path_string).unwrap();
+        output_file.write_all(key.as_slice());
     }
 
-    fn export_keys_to_file(&self, path: &Path) {
-        unimplemented!()
+    fn export_key_pair(&self, path: &Path) {
+        let mut public_path_string = path.to_str().unwrap().to_owned();
+        public_path_string.push_str(".pub");
+        let public_path = Path::new(public_path_string.as_str());
+        self.export_private_key(path);
+        self.export_public_key(public_path);
     }
 
     fn verify_signature(&self, data: &Message, signature: &Signature) -> bool {
