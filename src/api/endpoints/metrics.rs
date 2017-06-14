d5112496e228f53e05863da3b36c982a648b50c5
Valentin Fries <contact@fries.io>
2017-06-15 00:19:13 +0200

    Harmonized endpoints output and added an Identities Metric

--- src/api/endpoints/metrics.rs
+++ src/api/endpoints/metrics.rs
@@ -7,13 +7,15 @@ use blockchain::metric::*;
 
 use blockchain::block::BlockRepository;
 use blockchain::peer::PeerRepository;
+use blockchain::identity::IdentityRepository;
 
 pub fn get_all(req: &mut Request) -> IronResult<Response> {
     let connection = req.get_connection()?;
     
     let metrics = vec![
         get_blocks_metric(&*connection)?,
-        get_peers_metric(&*connection)?
+        get_peers_metric(&*connection)?,
+        get_identities_metric(&*connection)?
     ];
     
     response!(Ok, metrics)
@@ -33,6 +35,15 @@ fn get_peers_metric(connection: &SqliteConnection) -> IronResult<Metric<i64>> {
     
     match repository.count() {
         Ok(count) => Ok(Metric::new("Peers", count)),
+        Err(err) => error!(InternalServerError, {"error": err.description()})
+    }
+}
+
+fn get_identities_metric(connection: &SqliteConnection) -> IronResult<Metric<i64>> {
+    let repository = IdentityRepository::new(&connection);
+    
+    match repository.count() {
+        Ok(count) => Ok(Metric::new("Identities", count)),
         Err(err) => error!(InternalServerError, {"error": err.description()})
     }
 }
